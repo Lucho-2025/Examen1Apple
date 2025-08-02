e5c963f2d1bf1fd20e126f432e2d067661a0ac19
Lucho-2025 <luis.ancari@uab.edu.bo>
2025-08-01 22:16:49 -0400

    seeder de productos

--- backend/app/Models/Producto.php
+++ backend/app/Models/Producto.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     //
+    public function marca() {
+    return $this->belongsTo(Marca::class);
+    }
+
+    public function productos() {
+    return $this->hasMany(Producto::class);
+    }
 }
